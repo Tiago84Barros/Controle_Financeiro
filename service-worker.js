53ea05b16e5d07050b68a28ccda6451085f4449f
Tiago <tsbarros84@yahoo.com.br>
2025-11-26 03:48:45 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,20 +1,3 @@
-{
-  "name": "Meu Controle Financeiro",
-  "short_name": "Financeiro",
-  "start_url": "/",
-  "display": "standalone",
-  "background_color": "#000000",
-  "theme_color": "#000000",
-  "icons": [
-    {
-      "src": "/app_icon_192.png",
-      "sizes": "192x192",
-      "type": "image/png"
-    },
-    {
-      "src": "/app_icon_512.png",
-      "sizes": "512x512",
-      "type": "image/png"
-    }
-  ]
-}
+self.addEventListener("install", () => {
+  console.log("Service worker instalado");
+});
